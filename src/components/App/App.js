ae403e0e68e9df431d9eec48798870022d640fab
ayushhagarwal <ayushgrwl365@gmail.com>
2021-06-27 09:49:04 +0530

    building nameit react application

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../Header/Header';
+import SearchBox from '../SearchBox/SearchBox';
+import './App.css';
+
+class App extends React.Component {
+    state = {
+        headerText: 'Name It!',
+        headerExpanded: true,
+    };
+    handleInputChange = (inputText) => {
+        this.setState({ headerExpanded: !inputText });
+    };
+    render() {
+        return (
+            <div>
+                <Header
+                    headerExpanded={this.state.headerExpanded}
+                    headTitle={this.state.headerText}
+                />
+                <SearchBox onInputChange={this.handleInputChange} />
+            </div>
+        );
+    }
+}
+// function App() {
+//     return <h1> This is a functional component! </h1>;
+// }
+
+export default App;
